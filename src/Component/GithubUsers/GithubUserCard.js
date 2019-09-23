ec68d4c2f44a75eea2e5edb68a78718d34d249cc
Manju KC <kcmanjuthapa@gmail.com>
2019-09-23 14:31:27 -0400

    imported  GithubUserCardcomponent to the GithubUserList

--- src/Component/GithubUsers/GithubUserCard.js
+++ src/Component/GithubUsers/GithubUserCard.js
@@ -11,3 +11,4 @@ export default function GithubUserCard ({ char })  {
     </div>
   );
 };
+
---
+++ src/Component/GithubUsers/GithubUserList.js
@@ -0,0 +1,10 @@
+import React from "React";
+import GithubUserCard from "../GithubUsers"
+
+const GithubUserList = props => (
+    <div>
+     <GithubUserCard/>
+    </div>
+  )
+
+  export default GithubUserList;
