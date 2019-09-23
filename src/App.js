1e01ee535b7c892ef62eca64f20cf9c13048b7e2
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-09-23 18:43:46 +0100

    added API request / useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+    const API = 'https://developer.github.com/v3/'
+  
+    useEffect(() => {
+      axios.get(API)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error =>{
+        console.log("this is an error", error);
+      })
+    }, [])
+  
   return (
    <h1>
      Hello
