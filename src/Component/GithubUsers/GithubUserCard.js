08b3e0bf57f8e1757f339449b2da07c8a416650a
Manju KC <kcmanjuthapa@gmail.com>
2019-09-23 14:21:44 -0400

    created GithubUserCard component

--- src/Component/GithubUsers/GithubUserCard.js
+++ src/Component/GithubUsers/GithubUserCard.js
@@ -1,1 +1,13 @@
-import React from "react"
+import React from "react";
+
+export default function GithubUserCard ({ char })  {
+  return (
+    <div>
+      <img alt="avatar" style={{ width: "70px" }} src={props.char.avatar_url} />
+      <div>
+        <div style={{ fontWeight: "bold" }}>{props.char.name}</div>
+        <div>{props.char.blog}</div>
+      </div>
+    </div>
+  );
+};
