c07339097d61dba66170bc90a058212139f5b398
Manju KC <kcmanjuthapa@gmail.com>
2019-09-23 16:06:52 -0400

    fixed the error of GithubUserCard

--- src/Component/GithubUsers/GithubUserCard.js
+++ src/Component/GithubUsers/GithubUserCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function GithubUserCard ({ char })  {
+export default function GithubUserCard ({char})  {
   return (
     <div>
-      <img alt="avatar" style={{ width: "70px" }} src={props.char.avatar_url} />
+      <img alt="avatar" style={{ width: "70px" }} src={char.avatar_url} />
       <div>
-        <div style={{ fontWeight: "bold" }}>{props.char.name}</div>
-        <div>{props.char.blog}</div>
+        <div style={{ fontWeight: "bold" }}>{char.name}</div>
+        <div>{char.blog}</div>
       </div>
     </div>
   );
