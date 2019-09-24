64d660b882b28a043f80db5be4faef43981ecc63
Manju KC <kcmanjuthapa@gmail.com>
2019-09-24 13:49:14 -0400

    created GithubLanguList component

---
+++ src/Component/GithubRepoLangu/GithubLanguList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GithubLanguCard from "./githubLanguCard"
+
+ function CommitLanguList(props) {
+   console.log(props)
+   return (
+  <div>
+    <GithubLanguCard/>
+  </div>
+   )
+ }
+
+ export default CommitLanguList;
