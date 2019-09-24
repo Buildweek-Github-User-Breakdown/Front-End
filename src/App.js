3c1cdf9716ccf5fa8efbb4356beea360476b90c3
ren.meta@gmail.com <ren.meta@gmail.com>
2019-09-24 18:32:39 -0400

    Login functionality working, with added Contexts hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState, useContext} from "react";
 import SignUpForm from "./components/SignUpForm";
 import LoginForm from "./components/LogIn";
 import "./App.css";
 
+import { UserDataProvider } from './contexts/UserDataContext';
+
 function App() {
+  const [userData, setUserData] = useState({})
+
+
   return (
     <header>
+      <UserDataProvider value={{ userData, setUserData }}>
       <LoginForm />
+      {/* <SignUpForm /> */}
+    </UserDataProvider>
     </header>
   );
 }
--- src/components/AxiosWithAuth.js
+++ src/components/AxiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
 
     return axios.create({
-        baseURL: 'http://localhost:5000',
+        baseURL: 'https://github-user-breakdown.herokuapp.com/',
         headers:{
             "Content-Type": "application/json",
             "Authorization": `${token}`
