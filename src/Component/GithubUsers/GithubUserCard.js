7916c2c85f51032c8c5fb6235595b8560dc3df16
Manju KC <kcmanjuthapa@gmail.com>
2019-09-25 00:16:22 -0400

    changed styling of components

--- src/Component/GithubUsers/GithubUserCard.js
+++ src/Component/GithubUsers/GithubUserCard.js
@@ -10,20 +10,43 @@ border:2px solod grey;
 
 const Div = Styled.div`
 fontWeight:bold;
-font-size:25px;
-margin-left:70px;
-padding-top:40px;
+font-size:20px;
+margin-left:20px;
+
 
 `;
 const Images = Styled.img`
-margin-left:320px;
+margin-top:50px;
+height:180px;
+width:800px;
+margin-left:100px;
+border:2px solid grey;
+`;
+const ImgDiv = Styled.div`
+
 `;
 
+const Maindiv = Styled.div`
+display: flex;
+flex-direction: row;
+margin-top:50px;
+flex-wrap:wrap;
+`;
+const Imagesdiv = Styled.div`
+
+`
+const Languages = Styled.div`
+margin-left:200px;
+font-size:20px;
+margin-top:30px;
+`
+
 export default function GithubUserCard(props) {
-  console.log(props.lang)
+  console.log(props.lang);
   return (
-    <div>
-      <Img alt="avatar" src={props.avatar_url} />
+    <Maindiv>
+      <ImgDiv>
+        <Img alt="avatar" src={props.avatar_url} /> 
       <Div>
         <div>{props.bio}</div>
         <div>{props.name}</div>
@@ -32,12 +55,16 @@ export default function GithubUserCard(props) {
         <div>Followers: {props.followers}</div>
         <div>Following: {props.following}</div>
         <div>{props.html_url}</div>
+      </Div>
+      </ImgDiv>
+      <Imagesdiv>
         <Images
           src={"http://ghchart.rshah.org/" + props.login}
           alt="Github chart"
         />
-        <div>Popular language: { props.lang } </div>
-      </Div>
-    </div>
+        <Languages>Popular language: {props.lang} </Languages>
+      </Imagesdiv>
+      
+    </Maindiv>
   );
 }
--- src/Component/GithubUsers/GithubUsers.js
+++ src/Component/GithubUsers/GithubUsers.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import GithubUserList from "./GithubUserList"
 import Form from "./Form"
+import Styled from "styled-components";
+
+const H1 = Styled.h1`
+margin-left:460px;
+margin-top:20px;
+`
+
 
 const GithubUsers = () => {
   const [cards, setCards] = useState([]);
@@ -16,7 +23,7 @@ const GithubUsers = () => {
 
   return (
     <div>
-      <h1>GitHub User DashbOard</h1>
+      <H1>GitHub User Dashboard</H1>
       <Form onSubmit={addNewCard} addLanguage={addLanguage}/>
       <GithubUserList cards={cards} lang={lang}/>
     </div>
