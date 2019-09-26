db7784ac949f5a6d2abfe7bc169f73ae54bc3cf9
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-09-26 11:12:22 -0400

    refactor commitHistoryRepo

--- src/Component/GithubUsers/GithubUsers.js
+++ src/Component/GithubUsers/GithubUsers.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import GithubUserList from "./GithubUserList"
-import Form from "./Form"
+import GithubUserList from "./GithubUserList";
+import Form from "./Form";
 import Styled from "styled-components";
 import Header from "../Header";
 import CommitHistoryRepo from "../../Components/CommitHistoryRepo";
@@ -8,33 +8,30 @@ import CommitHistoryRepo from "../../Components/CommitHistoryRepo";
 const H1 = Styled.h1`
 margin-left:460px;
 margin-top:20px;
-`
-
-
+`;
 const GithubUsers = () => {
   const [cards, setCards] = useState([]);
   const [lang, setLang] = useState([]);
   const [repo, setRepo] = useState([]);
 
   const addNewCard = cardInfo => {
-    setCards(cards.concat(cardInfo))
-  }
+    setCards(cards.concat(cardInfo));
+  };
 
   const addLanguage = language => {
-    setLang(lang.concat(language))
-  }
+    setLang(lang.concat(language));
+  };
 
   const addRepo = repoPins => {
     setRepo(repo.concat(repoPins));
-  }
+  };
 
   return (
-
     <div className="wrapper">
       <h1>GitHub User DashbOard</h1>
-      <Form onSubmit={addNewCard} addLanguage={addLanguage} addRepo={addRepo}/>
-      <GithubUserList cards={cards} lang={lang}/>
-      <CommitHistoryRepo repo={repo}/>
+      <Form onSubmit={addNewCard} addLanguage={addLanguage} addRepo={addRepo} />
+      <GithubUserList cards={cards} lang={lang} />
+      <CommitHistoryRepo repo={repo} />
     </div>
   );
 };
